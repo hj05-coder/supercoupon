41dda8b8e55d1867fb7cc4fd2b8632aa1c213a35
itcast <hello@itcast.cn>
2025-08-14 18:11:44 +0800

    开发用户优惠券分发功能（二）

---
+++ distribution/src/main/java/com/hj/supercoupon/distribution/dao/mapper/CouponTaskFailMapper.java
@@ -0,0 +1,10 @@
+package com.hj.supercoupon.distribution.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hj.supercoupon.distribution.dao.entity.CouponTaskFailDO;
+
+/**
+ * 优惠券模板失败记录数据库持久层
+ */
+public interface CouponTaskFailMapper extends BaseMapper<CouponTaskFailDO> {
+}
---
+++ distribution/src/main/java/com/hj/supercoupon/distribution/mq/event/CouponTemplateDistributionEvent.java
@@ -0,0 +1,58 @@
+package com.hj.supercoupon.distribution.mq.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 优惠券模板任务执行事件
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CouponTemplateDistributionEvent {
+
+    /**
+     * 优惠券分发任务id
+     */
+    private Long couponTaskId;
+
+    /**
+     * 优惠券分发任务批量id
+     */
+    private Long couponTaskBatchId;
+
+    /**
+     * 店铺编号
+     */
+    private Long shopNumber;
+
+    /**
+     * 优惠券模板id
+     */
+    private Long couponTemplateId;
+
+    /**
+     * 有效期结束时间
+     */
+    private Date validEndTime;
+
+    /**
+     * 消耗规则
+     */
+    private String couponTemplateConsumeRule;
+
+    /**
+     * 批量保存用户优惠券 Set 长度，默认满 5000 才会批量保存数据库
+     */
+    private Integer batchUserSetSize;
+
+    /**
+     * 分发结束标识
+     */
+    private Boolean distributionEndFlag;
+}
